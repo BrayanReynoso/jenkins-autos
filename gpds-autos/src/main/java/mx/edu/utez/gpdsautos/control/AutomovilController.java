0d372feefe90b84d41644855fc3f733ad2b6e436
Brayan Reynoso Macedo <94414445+BrayanReynoso@users.noreply.github.com>
2024-11-29 15:36:24 -0600

    Update AutomovilController.java

--- gpds-autos/src/main/java/mx/edu/utez/gpdsautos/control/AutomovilController.java
+++ gpds-autos/src/main/java/mx/edu/utez/gpdsautos/control/AutomovilController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//modficacion de prueba
 @RestController
 @RequestMapping("/api/automoviles")
 public class AutomovilController {
